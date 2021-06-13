124aa7afd79772c4acb7459c4cb105e51c27ecd1
Yasmin Sales <teacheryasminsales@gmail.com>
2021-06-13 11:45:58 -0300

    Exercício de Linguagem de Programação

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace console_quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int quadrado = 0;
+
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            var calculadoraQuadrado = new Quadrado();
+            quadrado = calculadoraQuadrado.ComParametro(numero); 
+
+            Console.WriteLine("O quadrado do número é: " + quadrado);
+
+            calculadoraQuadrado.Valor = numero; 
+            quadrado = calculadoraQuadrado.SemParametro();
+            Console.WriteLine("O quadrado do número é: " + quadrado);
+            Console.ReadKey();
+
+    }
+    }
+}
---
+++ Quadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace console_quadrado
+{
+    class Quadrado
+    { 
+        public int Valor { get; set; }
+
+        public int ComParametro(int valor)
+        {
+            return valor * valor; 
+        }
+
+        public int SemParametro()
+        {
+            return Valor * Valor;
+        }
+    }
+}
